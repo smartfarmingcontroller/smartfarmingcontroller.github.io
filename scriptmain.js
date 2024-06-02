167e8b8c1f7082e610679f6ca96264d9362ae11b
smartfarmingcontroller <smartfarmingcontroller@gmail.com>
2024-06-02 16:12:58 -0300

    Add files via upload

---
+++ scriptmain.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    document.querySelectorAll('.fade-in').forEach(element => {
+        observer.observe(element);
+    });
+});
